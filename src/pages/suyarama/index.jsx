ced50f0d5df09134bf62048a6c4f839a9f36f58a
torhulugh <torhulugh@gmail.com>
2026-07-09 08:16:49 -0400

    created suyarama qc page added menu photo and mobile size

--- src/pages/suyarama/index.jsx
+++ src/pages/suyarama/index.jsx
@@ -6,25 +6,23 @@ export default function Header({ Suyarama }) {
     <>
       <section className="brand---q-c--page">
         <img id="brand--logo" src="/img/T.H.Asset 19vande.png" alt="" />
-        <h1>Suyarama</h1>
-        <h2 className="brand--info">
+        <h1 className="brand--h1"> Suyarama</h1>
+        <h3 className="brand--info">
           bring the rich flavors and vibrant culture of West African street food
           to Canada through authentic recipes, warm hospitality, and
           unforgettable dining experiences that make everyone feel at home.
-        </h2>
+        </h3>
 
-        <Link className="Brand--link" to="https://www.instagram.com/suya_rama/">
+        <Link className="brand--link" to="https://www.instagram.com/suya_rama/">
           <img src="/img/instagram-icn.svg" alt="" />
           <p>follow suyarama on Instagram</p>
         </Link>
-        <Link className="Brand--link" to="https://www.tiktok.com/@nguvendan4444">
+        <Link className="brand--link" to="https://www.tiktok.com/@nguvendan4444">
           <img src="" alt="" />
           <p>follow suyarama on TikTok</p>
         </Link>
-        <Link className="Brand--link" to="/suyarama">
-          <img src="" alt="" />
-          <p>Veiw Our menu</p>
-        </Link>
+        <h2>  Menu </h2>
+        <img className="menu--img" src="/img/sunfest.jpeg" alt="" />
       </section>
     </>
   );
