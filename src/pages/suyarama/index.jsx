855a87b6c9d6e8f7df33abe4253ab72113a20315
torhulugh <torhulugh@gmail.com>
2026-07-09 14:51:05 -0400

    created suyarama qc page added colored social icons and border fir brand--linkd

--- src/pages/suyarama/index.jsx
+++ src/pages/suyarama/index.jsx
@@ -5,22 +5,25 @@ export default function Header({ Suyarama }) {
   return (
     <>
       <section className="brand---q-c--page">
-        <img id="brand--logo" src="/img/T.H.Asset 19vande.png" alt="" />
+        <img className="brand--logo" src="/img/T.H.Asset 12vande.png" alt="" />
         <h1 className="brand--h1"> Suyarama</h1>
         <h3 className="brand--info">
           We bring the rich flavors and vibrant culture of West African street food
           to Canada through authentic recipes, warm hospitality, and
           unforgettable dining experiences that make everyone feel at home.
         </h3>
-
-        <Link className="brand--link" to="https://www.instagram.com/suya_rama/">
-          <img src="/img/instagram-icn.svg" alt="" />
+        <secttion className="brand--links--container">
+          <Link className="brand--link instagram" to="https://www.instagram.com/suya_rama/">
+          <img src="/img/instagram-true.png" alt="" />
           <p>follow suyarama on Instagram</p>
         </Link>
-        <Link className="brand--link" to="https://www.tiktok.com/@nguvendan4444">
-          <img src="" alt="" />
+        <Link className="brand--link tiktok" to="https://www.tiktok.com/@nguvendan4444">
+          <img src="/img/tiktok-true.png" alt="" />
           <p>follow suyarama on TikTok</p>
         </Link>
+        </secttion>
+
+        
         <h2 className="brand--h2" >  Menu </h2>
         <img className="menu--img" src="/img/sunfest.jpeg" alt="" />
       </section>
