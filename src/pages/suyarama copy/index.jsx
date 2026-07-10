b585b4863a2611f43c5f8ef1e4376d4992454a46
torhulugh <torhulugh@gmail.com>
2026-07-10 19:05:47 -0400

    created suyarama qc page added brand--linkd media-q repush

---
+++ src/pages/suyarama copy/index.jsx
@@ -0,0 +1,32 @@
+import "./style.css";
+import { Link } from "react-router-dom";
+
+export default function Header({ Suyarama }) {
+  return (
+    <>
+      <section className="brand---q-c--page">
+        <img className="brand--logo" src="/img/T.H.Asset 12vande.png" alt="" />
+        <h1 className="brand--h1"> Suyarama</h1>
+        <h3 className="brand--info">
+          We bring the rich flavors and vibrant culture of Nigerian street food
+          to Canada through authentic recipes, warm hospitality, and
+          unforgettable dining experiences that make everyone feel at home.
+        </h3>
+        <secttion className="brand--links--container">
+          <Link className="brand--link instagram" to="https://www.instagram.com/suya_rama/">
+          <img src="/img/instagram-true.png" alt="" />
+          <p>follow suyarama on Instagram</p>
+        </Link>
+        <Link className="brand--link tiktok" to="https://www.tiktok.com/@nguvendan4444">
+          <img src="/img/tiktok-true.png" alt="" />
+          <p>follow suyarama on TikTok</p>
+        </Link>
+        </secttion>
+
+        
+        <h2 className="brand--h2" >  Menu </h2>
+        <img className="menu--img" src="/img/sunfest.jpeg" alt="" />
+      </section>
+    </>
+  );
+}
